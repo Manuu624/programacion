96e6855966ddb3272c844961400692adf5f939b4
Manuu624 <crackseba@gmail.com>
2022-07-01 17:06:36 -0300

    a

--- Emmanuel Maldonado/array_map/main.js
+++ Emmanuel Maldonado/array_map/main.js
@@ -1,19 +1,31 @@
 const tareas = [
     { 
         nombre: 'Testeando soft',
-        duracion: '25min'
+        duracion: 25
     
     },
 
     {
         nombre: 'implementacion de alta cliente',
-        duracion: '90min'
+        duracion: 90
 
     },
 
     {
         nombre: 'actualiza cliente',
-        duracion: '40min'
+        duracion: 40
+
+    },
+
+    { nonmbre: 'deplay de soft',
+
+      duracion: 120
+    },
+    
+    {
+    nombre: 'correccion de bug',
+    duracion: 180
+
 
     }
 
@@ -33,3 +45,26 @@ tareas.map (tarea => {
     return  tarea.nombre
 
 })
+
+//uso de filter
+
+let tareas_prolongadas = []
+tareas.forEach(element => {
+
+    if (element.duracion>=129){
+            tareas_prolongadas.push(element)
+
+    }
+
+
+
+});
+console.log(tareas_prolongadas)
+
+//filtrar haciendo uso el metodo filter
+
+const tareas_prolongadas2 = tareas.filter( tarea => {
+    return tareas.duracion>=120
+
+})
+console.log(tareas_prolongadas2)
